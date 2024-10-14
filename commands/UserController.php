fba62765ebc5a87e37a27a9500983563225836de
Semenov Daniil <ds.daniilsemen.ds@gmail.com>
2024-10-14 12:50:51 +0300

    Create a UserController console controller to create users. Create an action in the UserController to create an expert

---
+++ commands/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\commands;
+
+use app\models\Competencies;
+use app\models\Users;
+use app\models\UsersCompetencies;
+use Yii;
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+class UserController extends Controller
+{
+    public $defaultAction = 'init';
+
+    /**
+     * This command creates user expert.
+     * 
+     * @return int Exit code
+     */
+    public function actionCreateExpert()
+    {
+        $model = new UsersCompetencies();
+        
+        $model->load([
+            'surname' => 'Main',
+            'name' => 'Expert',
+            'title' => 'Сompetence',
+            'num_modules' => 4
+        ], '');
+        
+        if ($model->validate()) {
+            $transaction = Yii::$app->db->beginTransaction();   
+            try {
+                $user = new Users(['scenario' => Users::SCENARIO_ADD_EXPERT]);
+                $user->attributes = $model->attributes;
+                $user->addExpert();
+
+                $competence = new Competencies();
+                $competence->attributes = $model->attributes;
+                $competence->users_id = $user->id;
+                $competence->save();
+                
+                $transaction->commit();
+                echo "\nExpert create\n";
+                return ExitCode::OK;
+            } catch(\Exception $e) {
+                $transaction->rollBack();
+            } catch(\Throwable $e) {
+                $transaction->rollBack();
+            }
+        }
+
+        return ExitCode::UNSPECIFIED_ERROR;
+    }
+}
