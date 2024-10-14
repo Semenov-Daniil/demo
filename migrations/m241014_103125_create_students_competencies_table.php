60ecfcacc0f25c12f2b5bbd12584a05f74959163
Semenov Daniil <ds.daniilsemen.ds@gmail.com>
2024-10-14 13:56:41 +0300

    Add migration creating a new students_competencies table

--- migrations/m241014_103125_create_students_competencies_table.php
+++ migrations/m241014_103125_create_students_competencies_table.php
@@ -7,14 +7,24 @@ use yii\db\Migration;
  */
 class m241014_103125_create_students_competencies_table extends Migration
 {
+    const TABLE_NAME = '{{%students_competencies}}';
+    const TABLE_NAME_USERS = '{{%users}}';
+    const TABLE_NAME_COMPETENCIES = '{{%competencies}}';
+
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->createTable('{{%students_competencies}}', [
-            'id' => $this->primaryKey(),
+        $this->createTable(self::TABLE_NAME, [
+            'students_id' => $this->primaryKey(),
+            'competencies_id' => $this->integer()->notNull(),
         ]);
+
+        $this->addForeignKey('fk-students_competencies-students_id', self::TABLE_NAME, 'students_id', self::TABLE_NAME_USERS, 'id', 'CASCADE', 'CASCADE');
+
+        $this->createIndex('students_competencies-competencies_id', self::TABLE_NAME, 'competencies_id');
+        $this->addForeignKey('fk-students_competencies-competencies_id', self::TABLE_NAME, 'competencies_id', self::TABLE_NAME_COMPETENCIES, 'users_id', 'CASCADE', 'CASCADE');
     }
 
     /**
@@ -22,6 +32,10 @@ class m241014_103125_create_students_competencies_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropTable('{{%students_competencies}}');
+        $this->dropForeignKey('fk-students_competencies-students_id', self::TABLE_NAME);
+        $this->dropForeignKey('fk-students_competencies-competencies_id', self::TABLE_NAME);
+        $this->dropIndex('students_competencies-competencies_id', self::TABLE_NAME);
+
+        $this->dropTable(self::TABLE_NAME);
     }
 }
