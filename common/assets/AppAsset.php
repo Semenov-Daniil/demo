2e54dd4cf2fdb6770569c930193cac3170e1d694
Semenov-Daniil <ds.daniilsemen.ds@gmail.com>
2025-03-04 09:47:09 +0300

    Resource cleansing

--- common/assets/AppAsset.php
+++ common/assets/AppAsset.php
@@ -12,35 +12,18 @@ class AppAsset extends AssetBundle
 {
     public $sourcePath = '@common/assets';
     public $css = [
-        // aos
-        // 'libs/aos/aos.css',
-
-        // 'libs/toastr/toastr.css',
         'libs/noty/lib/noty.css',
         'css/noty-team-vz.css',
-
         'css/bootstrap.min.css',
         'css/icons.min.css',
         'css/app.css',
         'css/custom.min.css',
-        
     ];
     public $js = [
         'libs/bootstrap/js/bootstrap.bundle.min.js',
         'libs/simplebar/simplebar.min.js',
-        'libs/node-waves/waves.min.js',
-        'libs/feather-icons/feather.min.js',
         'js/pages/plugins/lord-icon-2.1.0.js',
-
         'libs/noty/lib/noty.js',
-        
-        'libs/choices.js/public/assets/scripts/choices.min.js',
-        'libs/flatpickr/flatpickr.min.js',
-        'libs/apexcharts/apexcharts.min.js',
-        'libs/jsvectormap/jsvectormap.min.js',
-        'libs/jsvectormap/maps/world-merc.js',
-        'libs/swiper/swiper-bundle.min.js',
-        // 'js/pages/dashboard-ecommerce.init.js',
         'js/alert-toastify.js',
         'js/gridview.js',
         'js/app.js'
--- common/assets/LoginAsset.php
+++ common/assets/LoginAsset.php
@@ -12,29 +12,21 @@ class LoginAsset extends AssetBundle
 {
     public $sourcePath = '@common/assets';
     public $css = [
+        'libs/noty/lib/noty.css',
+        'css/noty-team-vz.css',
         'css/bootstrap.min.css',
         'css/icons.min.css',
-        'css/app.min.css',
-        'css/custom.min.css'
+        'css/app.css',
+        'css/custom.min.css',
     ];
     public $js = [
-        [
-            'js/layout.js',
-            'position' => View::POS_HEAD,
-        ],
         'libs/bootstrap/js/bootstrap.bundle.min.js',
         'libs/simplebar/simplebar.min.js',
-        'libs/node-waves/waves.min.js',
-        'libs/feather-icons/feather.min.js',
         'js/pages/plugins/lord-icon-2.1.0.js',
-
-        // 'https://cdn.jsdelivr.net/npm/toastify-js',
-
-        // 'js/toastify-js.js',
-        
-        'libs/choices.js/public/assets/scripts/choices.min.js',
-        'libs/flatpickr/flatpickr.min.js',
-        'js/pages/password-addon.init.js',
+        'libs/noty/lib/noty.js',
+        'js/alert-toastify.js',
+        'js/gridview.js',
+        'js/app.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
